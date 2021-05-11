8a5b6ddcb539b1fd3fd40b738bd9948ccb1cc072
sheepkill15 <simonarons15@gmail.com>
2021-05-11 16:23:07 +0300

    webes verzio

--- packages/web/src/index.tsx
+++ packages/web/src/index.tsx
@@ -1,6 +1,8 @@
-import { AppRegistry } from 'react-native'
+import { AppRegistry } from 'react-native';
 
-import App from 'components/src/App'
+import App from 'components/src/App';
+
+require("@babel/polyfill"); // this should be include at the top
 
 AppRegistry.registerComponent('bolyai_monorepo', () => App)
 AppRegistry.runApplication('bolyai_monorepo', {
---
+++ packages/web/webpack.config.js
@@ -0,0 +1,144 @@
+
+const path = require('path');
+const webpack = require('webpack');
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const TerserWebpackPlugin = require("terser-webpack-plugin");
+const OptimizeCssAssetsPlugin = require("optimize-css-assets-webpack-plugin");
+require("@babel/polyfill"); // necesarry
+
+const appDirectory = path.resolve(__dirname, './');
+
+// This is needed for webpack to compile JavaScript.
+// Many OSS React Native packages are not compiled to ES5 before being
+// published. If you depend on uncompiled packages they may cause webpack build
+// errors. To fix this webpack can be configured to compile to the necessary
+// `node_module`.
+const babelLoaderConfiguration = {
+  test: /\.(ts|js)x?$/,
+  // Add every directory that needs to be compiled by Babel during the build.
+  include: [
+    path.resolve(appDirectory, 'src'),
+    path.resolve(appDirectory, '../components/src'),
+    path.resolve(appDirectory, '../../node_modules')
+  ],
+  use: {
+    loader: "babel-loader",
+    options: {
+      presets: [
+        "@babel/preset-env",
+        "@babel/preset-react",
+        "@babel/preset-typescript",
+      ],
+    },
+  },
+};
+
+// This is needed for webpack to import static images in JavaScript files.
+const imageLoaderConfiguration = {
+  test: /\.(gif|jpe?g|png|svg)$/,
+  use: {
+    loader: 'url-loader',
+    options: {
+      name: '[name].[ext]'
+    }
+  }
+};
+
+const typescriptLoaderConfiguration = {
+  test: /\.tsx?$/,
+  use: 'ts-loader',
+  exclude: /node_modules/,
+};
+
+const cssLoaderConfiguration = {
+  test: /\.css$/,
+  use:  ['style-loader', 'css-loader']
+};
+
+module.exports = {
+  
+  entry: ['@babel/polyfill', path.resolve(appDirectory, 'src/index.tsx')],
+
+  // configures where the build ends up
+  output: {
+    filename: 'assets/js/[name].[contenthash:8].js',
+    path: path.resolve(appDirectory, 'dist'),
+    publicPath: '/'
+  },
+
+  // ...the rest of your config
+
+  module: {
+    rules: [
+      babelLoaderConfiguration,
+      imageLoaderConfiguration,
+      typescriptLoaderConfiguration,
+      cssLoaderConfiguration
+    ]
+  },
+
+  resolve: {
+    // This will only alias the exact import "react-native"
+    alias: {
+      'react-native$': 'react-native-web'
+    },
+    // If you're working on a multi-platform React Native app, web-specific
+    // module implementations should be written in files using the extension
+    // `.web.js`.
+    extensions: [ '.web.js', '.js', '.ts', '.tsx', '.web.ts', '.web.tsx' ]
+  },
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: 'assets/css/[name].[contenthash:8].css',
+      chunkFilename: 'assets/css/[name].[contenthash:8].chunk.css'
+    }),
+    new HtmlWebpackPlugin({
+      template: path.resolve(appDirectory, 'public/index.html'),
+      inject: true
+    })
+  ],
+  optimization: {
+    minimize: true,
+    minimizer: [
+      new TerserWebpackPlugin({
+        terserOptions: {
+          compress: {
+            comparisons: false
+          },
+          mangle: {
+            safari10: true
+          },
+          output: {
+            comments: false,
+            ascii_only: false
+          },
+          warnings: false
+        }
+      }),
+      new OptimizeCssAssetsPlugin()
+    ],
+    splitChunks: {
+      chunks: 'all',
+      minSize: 0,
+      maxInitialRequests: 20,
+      maxAsyncRequests: 20,
+      cacheGroups: {
+        vendors: {
+          test: /[\\/]node_modules[\\/]/,
+          name(module, chunks, cacheGroupKey) {
+            const packageName = module.context.match(
+              /[\\/]node_modules[\\/](.*?)([\\/]|$)/
+            )[1];
+            return `${cacheGroupKey}.${packageName.replace("@", "")}`;
+          }
+        },
+        common: {
+          minChunks: 2,
+          priority: -10
+        }
+      }
+    },
+    runtimeChunk: 'single'
+  }
+}
