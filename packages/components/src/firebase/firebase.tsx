4c44a10bd464a606df48559fa9ea4a1b23a0f685
sheepkill15 <simonarons15@gmail.com>
2021-05-11 17:29:02 +0300

    fontos fajlok

---
+++ packages/components/src/firebase/firebase.tsx
@@ -0,0 +1,16 @@
+import firebase from 'firebase/app';
+import 'firebase/database';
+import 'firebase/analytics';
+const firebaseConfig = {
+    apiKey: "AIzaSyCzO_ETR9augvKJ40__UDqV_HaQItuwq1I",
+    authDomain: "bolyais-app.firebaseapp.com",
+    databaseURL: "https://bolyais-app.firebaseio.com",
+    projectId: "bolyais-app",
+    storageBucket: "bolyais-app.appspot.com",
+    messagingSenderId: "303318641764",
+    appId: "1:303318641764:web:1684b3e2b72cbbc5ee4371"
+  };
+
+firebase.initializeApp(firebaseConfig);
+firebase.analytics();
+export const getDatabase = () =>  firebase.database();
