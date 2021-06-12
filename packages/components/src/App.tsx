3f0366cf3a3c47856746f6f5f9dd919b67393626
sheepkill15 <simonarons15@gmail.com>
2021-06-12 08:30:35 +0300

    eddigi probalkozasok

--- packages/components/src/App.tsx
+++ packages/components/src/App.tsx
@@ -1,20 +1,23 @@
-import 'react-native-gesture-handler';
+// import 'react-native-gesture-handler';
 import React from 'react';
-import NavStack from './NavStack';
+// import NavStack from './NavStack';
 
-import DataStore from './firebase/datastore';
-import SettingsStore from './settings/SettingsStore';
+// import DataStore from './firebase/datastore';
+// import SettingsStore from './settings/SettingsStore';
+import { Text, View } from 'react-native';
 
+console.log('Outside app');
 const App = () => {
-  return (
-    <>
-      <SettingsStore.Provider>
-        <DataStore.Provider>
-                <NavStack />
-        </DataStore.Provider>
-      </SettingsStore.Provider>
-    </>
-  );
+  console.log('Inside app');
+  return <View style={{width: '100%', height: '100%'}}><Text style={{color: 'black'}}>Hello</Text></View>
+    // <>
+    //   <SettingsStore.Provider>
+    //     <DataStore.Provider>
+    //             <NavStack />
+    //     </DataStore.Provider>
+    //   </SettingsStore.Provider>
+    // </>
+    
 };
 
 export default App;
